f7334991e10fd3d669871b8b940d8f7f5bde81b3
Sunny Zuo <32416882+sunny-zuo@users.noreply.github.com>
2017-11-05 12:42:44 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
+//Import the discord.js module
 const Discord = require('discord.js');
+//Creates instance of discord client
 const client = new Discord.Client();
 
+//Causes bot to start reacting to messages after ready is given
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
+//Event listener for messages
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+// Token of the bot from Heroku
 client.login(process.env.BOT_TOKEN);
