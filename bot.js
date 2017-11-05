a4c8458c5d31c44eff0ea1f548010890f7b6e8bc
Sunny Zuo <32416882+sunny-zuo@users.noreply.github.com>
2017-11-05 13:22:43 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 //Import the discord.js module
 const Discord = require('discord.js');
+const Cleverbot = require("cleverbot-node");
 //Creates instance of discord client
 const client = new Discord.Client();
+
+const clbot = new Cleverbot;
+clbot.configure({botapi: process.env.CLEVER_TOKEN});
 
 //Causes bot to start reacting to messages after ready is given
 client.on('ready', () => {
@@ -10,6 +14,10 @@ client.on('ready', () => {
 
 //Event listener for messages
 client.on('message', message => {
+    var messageSplit = message.content.split(" ");
+    for (var i = 0; i < messageSplit.length; i++) {
+        
+    }
     if (message.content === 'Hi BrightBot!') {
     	message.channel.send('Hi' + message.author);
   	}
