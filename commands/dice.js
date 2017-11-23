bded26b57f689684b7faf096a9ff837502c63f44
sunny-zuo <32416882+sunny-zuo@users.noreply.github.com>
2017-11-22 20:44:50 -0700

    testing

--- commands/dice.js
+++ commands/dice.js
@@ -3,18 +3,20 @@ exports.run = (client, message, argsArray) => {
 	var rollInfo = args.split("");
 	var rollCount = rollInfo[0];
 	var rollMax;
+	console.log("hello1");
 	if (rollInfo[1] === "d") {
-		rollInfo.splice(0,2)
+		console.log("hello2")
+		rollInfo.splice(0,2);
 		rollMax = rollInfo.join("");
 		for (var i = 1; i < rollCount; i++) {
 			var roll;
 			roll = Math.floor(Math.random() * rollMax) + 1;
 			message.channel.send("Roll " + i + ":" + roll).catch(console.error);
-			console.log("Roll " + i + ":" + roll);
+			console.log("Roll " + i + ": " + roll);
 		}
 	}
 	else {
-		message.channel.send("That is not a valid command.")
+		message.channel.send("That is not a valid command.").catch(console.error);
 	}
 
 }
