b66e40240374354d15a02d9c5b3b4d34cbb0c817
Sunny Zuo <32416882+sunny-zuo@users.noreply.github.com>
2017-11-22 21:04:56 -0700

    removed unneeded logging

--- commands/dice.js
+++ commands/dice.js
@@ -6,11 +6,9 @@ exports.run = (client, message, argsArray) => {
 	if (rollInfo[1] === "d") {
 		rollInfo.splice(0,2);
 		rollMax = rollInfo.join("");
-		console.log(rollMax);
 		for (var i = 0; i < rollCount; i++) {
 			var roll;
 			roll = Math.floor(Math.random() * rollMax) + 1;
-			console.log("Roll " + i + ": " + roll);
 			message.channel.send("Roll " + i + " is " + roll).catch(console.error);
 		}
 	}
