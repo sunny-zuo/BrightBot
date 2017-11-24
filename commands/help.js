ed22bacc3ed269cce8fe5912d4dc47af3f464cca
sunny-zuo <32416882+sunny-zuo@users.noreply.github.com>
2017-11-23 19:03:29 -0700

    build rich embed instead

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,11 @@
 exports.run = (client, message, args) => {
-	message.channel.send({embed: {
-		color: 3447003,
-		title: "__Commands__",
-		description: "Here is a list of all the commands the bot will respond to.",
-		fields: [{
-			name: "-help",
-			value: "Lists commands.",
-			inline: true
-		}, 
-		{
-			name: "-say",
-			value: "Bot will say what it is given.",
-			inline: true
-		},
-		{
-			name: "-dice",
-			value: "Roll dice. Use format 1d6 where 1 is number of dice, and 6 is the highest number."
-		}],
-    	footer: {
-      		icon_url: "https://image.flaticon.com/icons/svg/25/25231.svg",
-      		text: "Developed by darkstar#7270. View my code: https://github.com/sunny-zuo/BrightBot"
-    	}
-	}});
+	const embed = new Discord.RichEmbed()
+		.setTitle("__Commands__");
+		.setColor(0x0000FF);
+		.setDescription("Here is a list of all the commands the bot will respond to.");
+		.setFooter("Developed by darkstar#7270. View my code: https://github.com/sunny-zuo/BrightBot", "https://image.flaticon.com/icons/svg/25/25231.svg");
+		.addField("-help", "Lists commands.", true);
+		.addField("-say", "Bot will say what it is given", true);
+		.addField("-dice", "Roll dice. Use format 1d6 where 1 is the number of dice rolled, and 6 is the max number (1-6). Cannot roll more than 9 dice at a time.")
+	message.channel.send({embed});
 }
